5c1cd715d6e00a4e4c8fa7cb0056ba30d7d24033
Dan <dan.berdinskikh@mail.ru>
2023-02-14 21:34:36 +0300

    Add users/me/ functionl

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
-
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .models import User
 from .serializers import UserSerializer
 
@@ -7,3 +8,9 @@ from .serializers import UserSerializer
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    @action(detail=False, url_path='me')
+    def recent_user(self, request):
+        user = request.user
+        serializer = self.get_serializer(user)
+        return Response(serializer.data)
