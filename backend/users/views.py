cb87d8515821710e54d679c6ec93a24939abd5b0
Dan <dan.berdinskikh@mail.ru>
2023-02-17 21:20:17 +0300

    Add subscribe and unsubscribe functionality

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.generics import get_object_or_404
 from .models import User, Follow
 from .serializers import UserSerializer, SetPasswordSerializer, SubscriptionSerializer
 
@@ -13,7 +14,7 @@ class UserViewSet(viewsets.ModelViewSet):
     def recent_user(self, request):
         user = request.user
         serializer = self.get_serializer(user)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     
     @action(methods=['post',], detail=False, url_path='set_password')
     def set_password(self, request):
@@ -24,23 +25,40 @@ class UserViewSet(viewsets.ModelViewSet):
                 return Response({"current_password": ["Неверный пароль"]}, status=status.HTTP_400_BAD_REQUEST)
             user.set_password(serializer.data.get("new_password"))
             user.save()
-            return Response(status=status.HTTP_200_OK)
+            return Response(status=status.HTTP_204_NO_CONTENT)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
+    @action(methods=('post', 'delete'), detail=True)
+    def subscribe(self, request, pk):
+        user = request.user
+        author = get_object_or_404(User, id=pk)
+        if request.method == 'POST':
+            if user == author:
+                return Response({
+                    'errors': 'Вы не можете подписаться на самого себя'
+                }, status=status.HTTP_400_BAD_REQUEST)
+            if Follow.objects.filter(user=user, author=author).exists():
+                return Response({
+                    'errors': 'Вы уже подписаны на данного пользователя'
+                }, status=status.HTTP_400_BAD_REQUEST)
+            Follow.objects.create(user=user, author=author)
+            serializer = SubscriptionSerializer(author)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        if user == author:
+            return Response({
+                'errors': 'Вы не можете отписаться от самого себя'
+            }, status=status.HTTP_400_BAD_REQUEST)
+        follow = Follow.objects.filter(user=user, author=author)
+        if follow.exists():
+            follow.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response({
+            'errors': 'Вы уже отписались'
+        }, status=status.HTTP_400_BAD_REQUEST)
+
     @action(methods= ('get',), detail=False)
     def subscription(self, request):
         user = request.user
         data = User.objects.filter(following__user=user)
         serializer = SubscriptionSerializer(data, many=True)
-        return Response(serializer.data)
-        # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-
-class SubscriptionViewSet(viewsets.ModelViewSet):
-    serializer_class = SubscriptionSerializer
-
-    def get_queryset(self):
-        user = self.request.user
-        queryset = User.objects.filter(following__user=user)
-        return queryset
-
+        return Response(serializer.data, status=status.HTTP_200_OK)
