4d3d9723a57a3f703a0f57fd5ed4c9cffcb9688f
Dan <dan.berdinskikh@mail.ru>
2023-02-28 20:41:57 +0300

    Add downloading shopping cart

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -2,9 +2,11 @@ from rest_framework import viewsets, status, permissions
 from rest_framework.decorators import action
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
+from django.http.response import HttpResponse
 from .paginataion import PageLimitPagination
 from .permissions import IsOwnerOrReadOnly
-from .models import Tag, Recipe, Ingredient, Favorites, ShoppingCarts
+from .models import (Tag, Recipe, Ingredient, Favorites, ShoppingCarts,
+                     IngredientAmount)
 from .serializers import TagSerializer, RecipeSerializer, IngredientSerializer
 from users.serializers import ShortRecipeSerializer
 
@@ -78,3 +80,31 @@ class RecipeViewSet(viewsets.ModelViewSet):
             {'errors': 'Рецепта нет в корзине'},
             status=status.HTTP_400_BAD_REQUEST
         )
+
+    @action(methods=['get',], detail=False)
+    def download_shopping_cart(self, request):
+        user = request.user
+        ingredients = IngredientAmount.objects.filter(
+            recipe__in_cart__user=user
+        ).values_list('ingredients__name',
+                      'ingredients__measurement_unit', 'amount')
+        cart_str = 'Список покупок:\n\n'
+        cart = {}
+        for ing in ingredients:
+            name = ing[0]
+            if name not in cart:
+                cart[name] = {
+                    'measurement_unit': ing[1],
+                    'amount': ing[2]
+                }
+            else:
+                cart[name]['amount'] += ing[2]
+        for ing in cart:
+            amount = cart[ing]['amount']
+            unit = cart[ing]['measurement_unit']
+            cart_str += f'{ing}: {amount} {unit}\n'
+        filename = f'{user}_shopping_cart.txt'
+        response = HttpResponse(cart_str, content_type='text/plain')
+        response['Content-Disposition'] = ('attachment; filename={0}'
+                                           .format(filename))
+        return response
